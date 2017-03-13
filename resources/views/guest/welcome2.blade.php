1375e8be04dc63a061f25397104a8125dde5bc5d
Enes Witwit <Witwit@dhcp665-178.laptop-wlc.uni-heidelberg.de>
2017-03-13 16:18:53 +0100

    Changed Language.

--- resources/views/guest/welcome2.blade.php
+++ resources/views/guest/welcome2.blade.php
@@ -231,23 +231,28 @@
                                         </div>
                                     </div>
                                 </form>
+
+
                                 <form id="register-form" action="http://phpoll.com/register/process" method="post" role="form" style="display: none;">
                                     <div class="form-group">
-                                        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+                                        <input type="text" name="vorname" id="vorname" tabindex="1" class="form-control" placeholder="Vorname" value="">
                                     </div>
                                     <div class="form-group">
-                                        <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+                                        <input type="text" name="Nachname" id="Nachname" tabindex="1" class="form-control" placeholder="Nachname" value="">
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+                                        <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email" value="">
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+                                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Passwort">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Passwort Bestätigen">
                                     </div>
                                     <div class="form-group">
                                         <div class="row">
                                             <div class="col-sm-6 col-sm-offset-3">
-                                                <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+                                                <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Bestätigen">
                                             </div>
                                         </div>
                                     </div>
