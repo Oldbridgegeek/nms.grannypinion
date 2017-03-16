a177f4c6644315d0048da62e1f9dd71d57ac837b
Enes Witwit <witwitenes@gmail.com>
2017-03-16 22:33:45 +0100

    Implemented Stars Review for Profile View.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,23 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+        <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+        <!-- jQuery -->
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous"></script>
+
+        <!-- Fonts -->
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+        <!-- Rating Javascript -->
+        <script src="js/bootstrap-rating.min.js"></script>
 
         <!-- Scripts -->
         <script>
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -34,47 +34,46 @@
             <ul class="list-group" style="margin-top:2em;box-shadow: 1px 3px 2px 2px grey;">
                 <div class="post" style="background-color:#c1c2c3;">
 
-                <li class="list-group-item">
-                    <b>Feedback erstellt am: {{$review->created_at->format('d.m.Y')}}</b>
-                </li>
+
+                    <b>{{$review->created_at->format('d.m.Y')}}</b>
                 @if($review->stars_average != NULL)
                 <li class="list-group-item">
-                    Gesamteindruck: {{$review->stars_average}} von {{ config('review.max_stars') }}
+                    Gesamteindruck: <input type="hidden" class="rating" disabled="disabled" value="{{$review->stars_average}}"/> 
                 </li>
                 @endif
                 @if($review->stars_honesty != NULL)
                 <li class="list-group-item">
-                    Ehrlichkeit: {{$review->stars_honesty}} von {{ config('review.max_stars') }}
+                    Ehrlichkeit: <input type="hidden" class="rating" disabled="disabled" value="{{$review->stars_honesty}}"/>
                 </li>
                 @endif
                 @if($review->stars_reliability != NULL)
                 <li class="list-group-item">
-                    Zuverlässigkeit: {{$review->stars_reliability}} von {{ config('review.max_stars') }}
+                    Zuverlässigkeit: <input type="hidden" class="rating" disabled="disabled" value="{{$review->stars_reliability}}"/>
                 </li>
                 @endif
                 @if($review->stars_attractiveness != NULL)
                 <li class="list-group-item">
-                    Attraktivität: {{$review->stars_attractiveness}} von {{ config('review.max_stars') }}
+                    Attraktivität: <input type="hidden" class="rating" disabled="disabled" value="{{$review->stars_attractiveness}}"/>
                 </li>
                 @endif
                 @if($review->stars_fun != NULL)
                 <li class="list-group-item">
-                    Humor und Spaß: {{$review->stars_fun}} von {{ config('review.max_stars') }}
+                    Humor und Spaß: <input type="hidden" class="rating" disabled="disabled" value="{{$review->stars_fun}}"/>
                 </li>
                 @endif
                 @if($review->stars_kindness != NULL)
                 <li class="list-group-item">
-                    Freundlichkeit: {{$review->stars_kindness}} von {{ config('review.max_stars') }}
+                    Freundlichkeit: <input type="hidden" class="rating" disabled="disabled" value="{{$review->stars_kindness}}"/>
                 </li>
                 @endif
                 @if($review->stars_intelligence != NULL)
                 <li class="list-group-item">
-                    Intelligenz: {{$review->stars_intelligence}} von {{ config('review.max_stars') }}
+                    Intelligenz: <input type="hidden" class="rating" disabled="disabled" value="{{$review->stars_intelligence}}"/>
                 </li>
                 @endif
                 @if($review->feedback != NULL)
                 <li class="list-group-item">
-                    Feedback: {{$review->feedback}}
+                    {{$review->feedback}}
                 </li>
                 @endif
                 </div>
